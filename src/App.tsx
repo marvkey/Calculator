b95b3f32d80dd25be25e5bc607cdccf6c67e29ad
marvkey <marvnwadike1@gmail.com>
2021-12-26 15:16:11 -0800

    fucntioanlity done

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
+import React,{useState} from 'react';
 import "./Grid.css"
 
 class Calculator{
   Operation:string =" ";
   Num1:string =" ";
   Num2:string =" ";
- 
 
   chooseOperation(operation:string){
     this.Operation = operation;
   }
   append(value:string){
-    if(this.Operation ==" "){
+    if(this.Operation ===" "){
+      if(value === "."){
+        if(this.Num1.indexOf('.') === -1){
+          this.Num1 += value;
+        }
+        return ;
+      }
       this.Num1 += value;
       return;
     }
+
+    if(value === "."){
+      if(this.Num2.indexOf('.') === -1){
+        this.Num2 += value;
+      }
+      return ;
+    }
     this.Num2 += value;
+    return;
   }
   Compute():number{
     if(this.Operation === " " || this.Num1 === " " || this.Num2 === " ") return 0;
-    let num1 =  parseFloat(this.Num1);
+    let num1 = parseFloat(this.Num1);
     let num2 = parseFloat(this.Num2);
     let answer:number = 0;
     switch(this.Operation){
@@ -47,41 +60,52 @@ class Calculator{
   }
 }
 
+let calulator = new Calculator();
+
 function App() {
-  let calulator = new Calculator();
-  let answer:number =0;
+  const [currentValue,stateSetValue] = useState(" ");
+  const clear = () => {
+    calulator.clear()
+    stateSetValue(" ");
+  }
   const numberOnClick = (e:string) =>{
     calulator.append(e);
     if(calulator.Operation ===" "){
       console.log("Num1: "+calulator.Num1);
+      stateSetValue(calulator.Num1);
       return;
     }
+    stateSetValue(calulator.Num2);
     console.log("Num2: "+ calulator.Num2);
   }
  
+  const operationOnClick =(e:string) => {
+    calulator.chooseOperation(e);
+    stateSetValue(calulator.Operation)
+  }
   return (
-    
    <div className ="calculator">
      <div className="output">
-        {calulator.Num1}
+        {currentValue}
       </div>
       <div className="calculator-grid">
-        <button calc-operation onClick ={() => calulator.clear()}>C</button>
-        <button calc-number onClick={(e) => numberOnClick("1")}>1</button>
-        <button calc-number onClick={(e) => numberOnClick("2")}>2</button>
-        <button calc-number onClick={(e) => numberOnClick("3")}>3</button>
-        <button calc-number onClick={(e) => numberOnClick("4")}>4</button>
-        <button calc-number onClick={(e) => numberOnClick("5")}>5</button>
-        <button calc-number onClick={(e) => numberOnClick("6")}>6</button>
-        <button calc-number onClick={(e) => numberOnClick("7")}>7</button>
-        <button calc-number onClick={(e) => numberOnClick("8")}>8</button>
-        <button calc-number onClick={(e) => numberOnClick("9")}>9</button>
-        <button calc-number onClick={(e) => numberOnClick("0")}>0</button>
-        <button calc-operation onClick={(e) => calulator.chooseOperation("/")}>/</button>
-        <button calc-operation onClick={(e) => calulator.chooseOperation("-")}>-</button>
-        <button calc-operation onClick={(e) => calulator.chooseOperation("+")}>+</button>
-        <button calc-operation onClick={(e) => calulator.chooseOperation("*")}>*</button>
-        <button calc-operation onClick={() => answer =calulator.Compute()}>=</button>
+        <button calc-operation onClick ={() => clear()}>C</button>
+        <button calc-number onClick={() => numberOnClick("1")}>1</button>
+        <button calc-number onClick={() => numberOnClick("2")}>2</button>
+        <button calc-number onClick={() => numberOnClick("3")}>3</button>
+        <button calc-number onClick={() => numberOnClick("4")}>4</button>
+        <button calc-number onClick={() => numberOnClick("5")}>5</button>
+        <button calc-number onClick={() => numberOnClick("6")}>6</button>
+        <button calc-number onClick={() => numberOnClick("7")}>7</button>
+        <button calc-number onClick={() => numberOnClick("8")}>8</button>
+        <button calc-number onClick={() => numberOnClick("9")}>9</button>
+        <button calc-number onClick={() => numberOnClick("0")}>0</button>
+        <button calc-number onClick={() => numberOnClick(".")}>.</button>
+        <button calc-operation onClick={() => operationOnClick("/")}>/</button>
+        <button calc-operation onClick={() => operationOnClick("-")}>-</button>
+        <button calc-operation onClick={() => operationOnClick("+")}>+</button>
+        <button calc-operation onClick={() => operationOnClick("*")}>*</button>
+        <button calc-operation onClick={() => stateSetValue(calulator.Compute().toString())}>=</button>
       </div>
     </div>
 
